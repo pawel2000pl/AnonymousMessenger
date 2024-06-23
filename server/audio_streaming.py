dc50e42238186b1f8c34d24dbcd0f6c9c145b56d
Paweł Bielecki <pawelek24@op.pl>
2024-06-23 12:58:41 +0200

    Optimalization: reduced buffer copying by replacing them with iterators

--- server/audio_streaming.py
+++ server/audio_streaming.py
@@ -1,13 +1,16 @@
 import os
+import math
 import json
 import cherrypy
 import warnings
 import messenger
+import itertools
 import numpy as np
 
 from time import sleep, time
 from scipy.io import wavfile
 from threading import Thread, Lock
+from messenger_logs import log_error
 from ws4py.websocket import WebSocket
 from ws4py.messaging import Message, BinaryMessage, TextMessage
 
@@ -42,6 +45,9 @@ LOW_NOTE = load_wave(LOW_NOTE_FILENAME, 0.3)
 HIGH_NOTE = load_wave(HIGH_NOTE_FILENAME, 0.3)
 ENTER_NOTIFICATION = LOW_NOTE + HIGH_NOTE
 LEAVE_NOTIFICATION = HIGH_NOTE + LOW_NOTE
+
+COMPRESSIONS_MAPS = tuple([list(itertools.repeat(1, AUDIO_BUF_SIZE))] + [[1 if j % i else 0 for j in range(AUDIO_BUF_SIZE)] for i in np.unique(np.logspace(2, 0, 64, dtype=int))[::-1]])
+COMPRESSIONS_SIZES = tuple(tuple(itertools.chain([0], itertools.accumulate(cmap))) for cmap in COMPRESSIONS_MAPS)
 
 class AudioThread(Thread):
 
@@ -79,12 +85,10 @@ class AudioThread(Thread):
                 for i, connection in enumerate(connections):
                     buffer[i] = connection.get_buffer_to_send(send_size)
 
-                if np.sum(np.abs(buffer)) == 0:
+                if not buffer.any():
                     continue
                 buffer = np.sum(buffer, axis=0) - buffer
-                buffer[buffer > 127] = 127
-                buffer[buffer < -127] = -127
-                buffer[buffer[:, 0] == ord('{'), 0] = ord('{')-1
+                buffer.clip(-127, 127, out=buffer)
                 buffer = buffer.astype(np.int8)
                 for i, connection in enumerate(connections):
                     if len(connection.buffers_to_send) < 5 and np.sum(np.abs(buffer[i])) != 0:                        
@@ -109,7 +113,8 @@ class AudioStreamWebSocketHandler(WebSocket):
         self.token = ""
         self.thread_id = 0
         self.lock = Lock()
-        self.buffer = ENTER_NOTIFICATION.copy()
+        self.recv_buffers = [(i for i in ENTER_NOTIFICATION)]
+        self.buffer_length = len(ENTER_NOTIFICATION)
         self.is_closed = False
         self.buffers_to_send = []
         self.connectTime = time()
@@ -134,10 +139,10 @@ class AudioStreamWebSocketHandler(WebSocket):
                     self.send(buf, binary=True)        
                 except (KeyboardInterrupt, SystemExit):
                     break
-                except (AttributeError, TimeoutError): # raised when connection is already closed
+                except (AttributeError, TimeoutError):
                     self.close()
                     break
-                except BrokenPipeError: # raised when connections are closed but because of SIGTERM
+                except BrokenPipeError:
                     AUDIO_THREADS[self.thread_id].connections = set()
                     break
                 except Exception as err:
@@ -169,20 +174,16 @@ class AudioStreamWebSocketHandler(WebSocket):
             elif self.thread_id:    
                 dest_size = AUDIO_SAMPLE_RATE * AUDIO_THREADS[self.thread_id].true_latency
                 with self.lock:
-                    buffer_len = len(self.buffer)
-                    add_size = min(AUDIO_BUF_SIZE-buffer_len, len(data))
-                    if add_size == 0:
+                    add_size = min(AUDIO_BUF_SIZE-self.buffer_length, len(data))
+                    if add_size <= 0:
                         return
-                    if 1.618 * dest_size > buffer_len:
-                        self.buffer.extend(data[:add_size])
-                    elif 2.618 * dest_size > buffer_len:
-                        self.buffer.extend(value for i, value in enumerate(data[:add_size]) if i & 15)
-                    elif 4.235 * dest_size > buffer_len:
-                        self.buffer.extend(value for i, value in enumerate(data[:add_size]) if i & 7)
-                    elif 6.854 * dest_size > buffer_len:
-                        self.buffer.extend(value for i, value in enumerate(data[:add_size]) if i & 3)
-                    else:
-                        self.buffer.extend(data[:add_size:2])                        
+
+                    compression_index = max(0, int(math.log((1+self.buffer_length) / dest_size, 1.618)))                    
+                    if compression_index < len(COMPRESSIONS_MAPS):
+                        compression_map = COMPRESSIONS_MAPS[compression_index]                        
+                        self.recv_buffers.append(itertools.compress(itertools.islice(data, add_size), compression_map))
+                        self.buffer_length += COMPRESSIONS_SIZES[compression_index][add_size]
+                                       
 
         except Exception as err:
             log_error(err)
@@ -190,17 +191,22 @@ class AudioStreamWebSocketHandler(WebSocket):
 
     def get_buffer_to_send(self, send_size):
         with self.lock:
-            if send_size > len(self.buffer):
+            if send_size > self.buffer_length:
                 return 0
-            send_buffer = np.fromiter((self.buffer.pop(0) for _ in range(send_size)), dtype=np.uint8).astype(np.int8)
+            self.buffer_length -= send_size
+            buffer_iter = itertools.chain.from_iterable(self.recv_buffers) if len(self.recv_buffers) > 1 else self.recv_buffers[0]
+            self.recv_buffers = [buffer_iter]
+            send_buffer = np.fromiter(buffer_iter, count=send_size, dtype=np.uint8).astype(np.int8, copy=False)
         return send_buffer
         
 
     def closed(self, code, reason=""):
         cherrypy.log('Audio connection %s closed'%self.userhash[:8])
         self.is_closed = True
-        self.buffer = LEAVE_NOTIFICATION.copy()
-        sleep(2 * len(self.buffer) / AUDIO_SAMPLE_RATE)
+        with self.lock:
+            self.buffer_length += len(LEAVE_NOTIFICATION)
+            self.recv_buffers.append(LEAVE_NOTIFICATION)
+        sleep(2 * self.buffer_length / AUDIO_SAMPLE_RATE)
         my_thread = AUDIO_THREADS[self.thread_id]
         if my_thread is not None:
             my_thread.connections.remove(self)
